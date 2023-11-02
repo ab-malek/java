afb597c3b0ee3cdfee5364e8c8a8d92211840770
Abdul Malek <109610531+ab-malek@users.noreply.github.com>
2023-11-02 22:04:31 +0600

    Update SimpleClientServer.java

--- SimpleClientServer.java
+++ SimpleClientServer.java
@@ -42,26 +42,3 @@ public class client {
 
     }
 }
-import java.net.*;
-import java.io.*;
-import java.util.*;
-
-public class client {
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
-        System.out.println("Client started");
-        Socket socket = new Socket("localhost",4545);
-
-        DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
-        DataInputStream dis = new DataInputStream(socket.getInputStream());
-
-        Scanner sc  = new Scanner(System.in);
-        String name = sc.nextLine();
-
-        dos.writeUTF(name);
-
-        name = dis.readUTF();
-
-        System.out.println("From server : "+name);
-
-    }
-}
