3d90d4e5fdb271cc503c9f6c72d724dd6087b4c0
Abdul Malek <109610531+ab-malek@users.noreply.github.com>
2023-11-04 21:46:37 +0600

    Update CustomException.java

--- CustomException.java
+++ CustomException.java
@@ -1,1 +1,30 @@
 
+import javax.swing.*;
+import java.awt.*;
+
+// For outof range 2147483647
+
+class negative extends Exception {
+    negative(String e){
+        super(e);
+    }
+}
+public class Main{
+
+    public static void main(String[] args){
+
+
+        try{
+            int a = -5;
+
+            if(a<0){
+                throw new negative("Number is negative");
+            }
+
+        }
+        catch (negative e){
+           System.out.println(e.getMessage());
+           System.out.println("Malke");
+        }
+    }
+}
